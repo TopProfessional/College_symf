20aad13cd1bb52bff674842d55ae9a8aa037515a
TopProfessional <d.od@ukr.net>
2022-01-05 17:20:46 +0200

    first_assets

--- src/Form/UserClassType.php
+++ src/Form/UserClassType.php
@@ -24,7 +24,6 @@ class UserClassType extends AbstractType
                     'multiple' => true,
                 ]
             );
-
     }
 
     public function configureOptions(OptionsResolver $resolver): void
