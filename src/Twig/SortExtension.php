1d8f174bf3a175645bca8bd9a9502d983fe86ecc
TopProfessional <d.od@ukr.net>
2022-01-27 12:56:07 +0200

    sort_for_review

--- src/Twig/SortExtension.php
+++ src/Twig/SortExtension.php
@@ -4,13 +4,21 @@ namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class SortExtension extends AbstractExtension
 {
-    public function getFunctions()
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function getFunctions(): array
     {
         return [
-            new TwigFunction('generate', [$this, 'generateSortUrl']),
+            new TwigFunction('generateSortUrl', [$this, 'generateSortUrl']),
         ];
     }
 
@@ -22,23 +30,37 @@ class SortExtension extends AbstractExtension
      *
      * @return string
      */
-    public function generateSortUrl(string $param, string $sort, string $curUrl, string $responseUrl): string
+    public function generateSortUrl(string $param, string $sort, string $curUrl): string
     {
-        $parseUrl = parse_url($curUrl);
+        $request = $this->requestStack->getMasterRequest();
+        $parseUrl = parse_url(/*$request*/$curUrl);
+        $responseUrl = '';
+        $output = [];
+        
+        $path = $parseUrl['path'];
+        // dd($ji);
 
         if(isset($parseUrl['query'])){
-
+            
             parse_str($parseUrl['query'], $output);
-            $output['field']=$param;
-            $output['sort']=$sort;
-            $responseUrl = 'users?'.http_build_query($output);
+
+            // $output['field']=$param;
+            // $output['sort']=$sort;
+            $responseUrl = $this->addParamToUrl($path,$param, $sort, $output);
         } else {
 
-            $output['field']=$param;
-            $output['sort']=$sort;
-            $responseUrl = 'users?'.http_build_query($output);
+            // $output['field']=$param;
+            // $output['sort']=$sort;
+            $responseUrl = $this->addParamToUrl($path,$param, $sort, $output);
         }
 
         return  $responseUrl; 
+    }
+
+    private function addParamToUrl(string $path,string $param, string $sort, array $output)
+    {
+        $output['field']=$param;
+        $output['sort']=$sort;
+        return /*'users?'*/$path.'?'.http_build_query($output);
     }
 }
