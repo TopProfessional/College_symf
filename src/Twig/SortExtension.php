ff8abc835738ff237222b480dddfd63f417c0c50
TopProfessional <d.od@ukr.net>
2022-01-28 16:49:09 +0200

    sorting_with_arrow

--- src/Twig/SortExtension.php
+++ src/Twig/SortExtension.php
@@ -2,11 +2,12 @@
 
 namespace App\Twig;
 
+use Doctrine\Common\Collections\Criteria;
 use Symfony\debug\Exception\FatalErrorException;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
-use Doctrine\Common\Collections\Criteria;
+
 
 class SortExtension extends AbstractExtension
 {
@@ -21,73 +22,57 @@ class SortExtension extends AbstractExtension
     {
         return [
             new TwigFunction('generateSortUrl', [$this, 'generateSortUrl']),
-            new TwigFunction('generateArrow', [$this, 'generateArrow']),
+            new TwigFunction('getArrowDirection', [$this, 'getArrowDirection']),
         ];
     }
 
     /**
      * @param string $param - sort atribure
-     * @param string $sort - order by
      *
      * @return string
      */
     public function generateSortUrl(string $param): string
     {
         $sort = Criteria::ASC;
-        //
-        // $request = $this->requestStack->getMasterRequest();
-        // $currentUrl = $request->getUri();  
-
-        // try { 
-        //     if(is_null($currentUrl)) {
-        //         throw new FatalErrorException('undefined url');
-        //     }
-        // } catch ( \Exception $e) {
-        //     echo 'Exception is: ',  $e->getMessage(), "\n";
-        // }
-        
-        // $parseUrl = parse_url($currentUrl);
-        //
         $parseUrl = $this->parseUrl();
         $output = [];
         $path = $parseUrl['path'];
 
         if(isset($parseUrl['query'])) {
             parse_str($parseUrl['query'], $output);
-            if($output['sort'] === $sort && $output['field'] === $param) {
-                $sort = Criteria::DESC;
+
+            if(isset($output['sort'])) {
+
+                if(strtoupper($output['sort']) === $sort && $output['field'] === $param) {
+                    $sort = Criteria::DESC;
+                }
             }
         } 
-        
         return $this->addParamToUrl($path, $param, $sort, $output);
     }
 
-    public function generateArrow(string $param): ?string
+    /**
+     * @param string $param - sort atribure
+     *
+     * @return string|null
+     */
+    public function getArrowDirection(string $param): ?string
     {
-        //
-        // $request = $this->requestStack->getMasterRequest();
-        // $currentUrl = $request->getUri();
-
-        // try { 
-        //     if(is_null($currentUrl)) {
-        //         throw new FatalErrorException('undefined url');
-        //     }
-        // } catch ( \Exception $e) {
-        //     echo 'Exception is: ',  $e->getMessage(), "\n";
-        // }
-
-        // $parseUrl = parse_url($currentUrl);
-        //
         $parseUrl = $this->parseUrl();
         if(isset($parseUrl['query'])) {
             parse_str($parseUrl['query'], $output);
-            if($output['sort'] === Criteria::DESC && $output['field'] === $param) {
-                return 'down';
-            } elseif ($output['sort'] === Criteria::ASC && $output['field'] === $param) {
-                return 'up';
+
+            if(isset($output['sort'])) {
+
+                if(strtoupper($output['sort']) === Criteria::DESC && $output['field'] === $param) {
+
+                    return 'down';
+                } elseif (strtoupper($output['sort']) === Criteria::ASC && $output['field'] === $param) {
+
+                    return 'up';
+                }
             }
         } 
-
         return null;
     }
 
